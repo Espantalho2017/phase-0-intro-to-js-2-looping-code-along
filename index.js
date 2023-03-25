04e27219eb3551120dd40d512584f898bfa3f8a9
Roscoe Moore <rmoore@peersat.com>
2023-03-25 15:53:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+let message = [];
+
+function writeCards(name, eventName) {
+    for(let i = 0; i < name.length ; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    } ; 
+
+    console.log(message);
+    return message; 
+};
+
+writeCards();
+
+
+function countDown(posInteger) {
+    console.log(posInteger);
+    while(posInteger > 0) {
+    posInteger -- ;
+    console.log(posInteger);
+    }
+}
+
+countDown();
+
